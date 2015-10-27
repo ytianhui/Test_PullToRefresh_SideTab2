11ed72dfd7de6d707fa1e42af0fee6026ed73142
ytianhui2004@gmail.com <yth220517>
2015-10-27 17:08:58 +0900

    Initial commit

---
+++ PullToRefresh_SideTabLayout/src/main/java/com/markmao/pulltorefresh/enterance/MainActivity_Slide.java
@@ -0,0 +1,58 @@
+package com.markmao.pulltorefresh.enterance;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
+import android.view.MenuItem;
+
+import com.markmao.pulltorefresh.R;
+import com.markmao.pulltorefresh.api.SlidingTabLayout;
+import com.markmao.pulltorefresh.slidetablayout.TabViewPagerAdapter;
+
+
+public class MainActivity_Slide extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        viewPager.setAdapter(new TabViewPagerAdapter(getSupportFragmentManager(),
+                MainActivity_Slide.this));
+        SlidingTabLayout slidingTabLayout = (SlidingTabLayout) findViewById(R.id.sliding_tabs);
+        // slidingTabLayout.setDistributeEvenly(true); 是否填充满屏幕的宽度
+        slidingTabLayout.setViewPager(viewPager);
+
+        //自定义下划线颜色
+        slidingTabLayout.setCustomTabColorizer(new SlidingTabLayout.TabColorizer(){
+            @Override
+            public int getIndicatorColor(int position) {
+                return Color.RED;
+            }
+        });
+
+    }
+/*
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+*/
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ PullToRefresh_SideTabLayout/src/main/java/com/markmao/pulltorefresh/slidetablayout/TabViewPagerAdapter.java
@@ -0,0 +1,36 @@
+package com.markmao.pulltorefresh.slidetablayout;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * 简单实例
+ * @author fyales
+ */
+public class TabViewPagerAdapter extends FragmentPagerAdapter {
+
+    private String mTabTitle[] = new String[]{"朝代", "人物", "战争"};
+    private Context mContext;
+
+    public TabViewPagerAdapter(FragmentManager fm, Context context) {
+        super(fm);
+        this.mContext = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return BaseFragment.newInstance(position);
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mTabTitle[position];
+    }
+}
